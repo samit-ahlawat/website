e47cb86df5b4548d677a4f9345635f0cfbab44b0
samit-ahlawat <85273069+samit-ahlawat@users.noreply.github.com>
2026-04-16 00:48:49 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import "./styles.css";
 
 function App() {
+  const [activeView, setActiveView] = useState("home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar setActiveView={setActiveView} />
+
+      {activeView === "dashboard" && <Dashboard />}
     </div>
   );
 }
